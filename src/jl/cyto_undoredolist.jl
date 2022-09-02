1e5ce7b4e5716ac303dbe4bdb1bc04156aaee89d
dizpatcher <dizpatcher@outlook.com>
2022-09-03 01:07:59 +0300

    undoredolist edited for correct index catching

--- src/jl/cyto_undoredolist.jl
+++ src/jl/cyto_undoredolist.jl
@@ -12,10 +12,12 @@ Expected usage is for undo/redo list demonstration.
 Keyword arguments:
 - `id` (String; optional): The ID used to identify this component in Dash callbacks.
 - `actions` (Array of Strings; optional): An array of history actions
+- `n_clicks` (Real; optional): Amount of clicks on the list
+- `n_clicks_timestamp` (Real; optional): Time of click on the list
 - `undo_index` (Real; optional): Index of event, start of which we will undo
 """
 function cyto_undoredolist(; kwargs...)
-        available_props = Symbol[:id, :actions, :undo_index]
+        available_props = Symbol[:id, :actions, :n_clicks, :n_clicks_timestamp, :undo_index]
         wild_props = Symbol[]
         return Component("cyto_undoredolist", "UndoRedoList", "dash_cytoscape", available_props, wild_props; kwargs...)
 end
